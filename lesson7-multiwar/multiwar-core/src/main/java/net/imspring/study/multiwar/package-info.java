bbd4d2d51d8574c2e726866592a45a5e3aca437e
imspring <cxc3214@163.com>
2015-09-17 09:18:34 +0800

    多war的项目完成

---
+++ lesson7-multiwar/multiwar-core/src/main/java/net/imspring/study/multiwar/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Administrator
+ *
+ */
+package net.imspring.study.multiwar;
---
+++ lesson7-multiwar/multiwar-core/src/main/java/net/imspring/study/multiwar/test.java
@@ -0,0 +1,7 @@
+package net.imspring.study.multiwar;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("核心业务");
+	}
+}
